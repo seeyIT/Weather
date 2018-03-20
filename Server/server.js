9e0dceb859b03d2686d938c1c9c9c1717288f1d8
seeyIT <lordkornelseeyit@gmail.com>
2018-03-20 10:50:32 +0100

    Register
    Register works

--- Server/server.js
+++ Server/server.js
@@ -12,55 +12,75 @@ const server = http.createServer((req, res) => {
 	var url_parts = url.parse(req.url, true);
 	var query = url_parts.query;
 	if(query.login != undefined){
-		insert(query.login, query.password);
-		
+		checkIfUserExists(query.login, function (result){
+			if(result){
+				insertUser(query.login,query.password);
+				res.end('1');
+			}
+			else{
+				console.log("user exists");
+				res.end('0');
+			}
+		})
 	}
 	
-	res.end('Hello World!');
 });
 
 
 	 
-function insert(login, password) {	
+function checkIfUserExists(login, callback) {	
 	MongoClient.connect("mongodb://localhost:27017/weather", function(err, db) {
+		if(err){ 
+			return console.dir(err); 
+		}
+
+		var dbo = db.db("weather");
+	  	var collection = dbo.collection('users');
+		 //check whether such user exists
+		collection.findOne( {"login" : login }, 
+			function(err, result) {
+				if (err) {
+			    	throw err;
+			    }
+			    if(result == null) {
+					callback(true);
+				}
+		    	else {    		
+			    	callback(false);  	
+			    }	    
+			});
+		db.close();
+	});	 
+}
+
+function insertUser(login, password) {	
+	MongoClient.connect("mongodb://localhost:27017/weather", function(err, db) {
+		if(err) { 
+			return console.dir(err); 
+		}
+
+		var dbo = db.db("weather");
+	  	var collection = dbo.collection('users');
+		var user = {"login": login,"password": password};	 
+
+		 //insert user
+		collection.insertOne(user, function(err, res) {
+		    if (err) {
+		    	throw err;
+		    }
+		    console.log("user added");
+		});		
+		db.close();
+	});
+}
+
+//init database and table
+MongoClient.connect("mongodb://localhost:27017/weather", function(err, db) {
 	if(err){ 
 		return console.dir(err); 
 	}
 
 	var dbo = db.db("weather");
-  	var collection = dbo.collection('users');
-	var user = {"login": login,"password": password};	 
-	  
-	 //check whether such user exists
-	collection.findOne( {"login" : login }, 
-		function(err, result) {
-			if (err) {
-		    	throw err;
-		    }
-		    console.log(result);	
-		    if(result == null) {
-		    	//TO DO make schedule query
-			// dbo.collection('users').insert(user, {w: 1}, function(err, records){
-				// 		console.log(err);
-				// });
-			}
-	    	else {    		
-		    	resultValue = "2323"; 
-		    	console.log(resultValue);   	    	
-		    }	    
-		});
-	db.close();
-
-	});	 
-}
-
-//init database and table
-MongoClient.connect("mongodb://localhost:27017/weather", function(err, db) {
-if(err){ 
-	return console.dir(err); 
-}
-
-var dbo = db.db("weather");
 	dbo.createCollection("users", function(err, res) {
 	    if (err) {
 	    	throw err;
